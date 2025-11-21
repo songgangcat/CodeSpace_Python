892839cf4170c4e2702757d264aee91f581903a9
songgangcat <ljhkys0130@gmail.com>
2025-11-21 05:02:03 +0000

    1929번 풀이

---
+++ BakJun/Python/Silver/1929.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+def sieve(n):
+    is_prime = [True] * (n+1)
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, int(n**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i*i, n+1, i):
+                is_prime[j] = False
+
+    return is_prime
+
+M, N = map(int, input().split())
+prime = sieve(N)
+
+for i in range(M, N+1):
+    if prime[i]:
+        print(i)
