38448c4b70139e69b0fe0321e8d4f31cb72851c0
songgangcat <ljhkys0130@gmail.com>
2025-11-24 14:02:04 +0000

    10773번 풀이

---
+++ BakJun/Python/Silver/10773.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+stack = []
+K = int(input())
+for _ in range(K):
+    N = int(input())
+    if N == 0:
+        stack.pop()
+    else:
+        stack.append(N)
+
+print(sum(stack))
