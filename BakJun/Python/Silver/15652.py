17455db8861a1841afe579ce59e8c93faad26c2f
songgangcat <ljhkys0130@gmail.com>
2025-12-15 13:34:18 +0000

    15652번 풀이

---
+++ BakJun/Python/Silver/15652.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+def backtrack(depth):
+    if depth == M:
+        print(*path)
+        return
+
+    for i in range(1, N+1):
+        if path and i < path[-1]:
+            continue
+        path.append(i)
+        backtrack(depth + 1)
+        path.pop()
+
+N, M = map(int, input().split())
+path = []
+backtrack(0)
